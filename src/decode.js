05231df6035743fad9fb43b8dd87d2c7b7c9c1e4
Ty J Everett <ty@tyweb.us>
2024-02-06 10:24:39 -0800

    Cleaner PusuDrop Decode

--- src/decode.js
+++ src/decode.js
@@ -15,61 +15,59 @@ const OP_2DROP = 109
  * @returns {Object} The decoded object, containing `fields`, `signature` and `lockingPublicKey`
  */
 module.exports = ({ script, fieldFormat = 'hex' }) => {
-  try {
-    // Check for non-hexadecimal characters
-    const hexchars = /[^0-9A-Fa-f][^0-9A-Fa-f]/
+  // Check for non-hexadecimal characters
+  const hexchars = /[^0-9A-Fa-f][^0-9A-Fa-f]/
 
-    if (script.match(hexchars) === null) {
-      const parsedScript = new bsv.Script(Buffer.from(script, 'hex'))
-      const lockingPublicKey = parsedScript.chunks[0].buf.toString('hex')
-      const fields = []
-      const acceptedFormats = ['hex', 'base64', 'utf8', 'buffer']
-      let signature
-      for (let i = 2; i < parsedScript.chunks.length; i++) {
-        const nextOpcode = parsedScript.chunks[i + 1].opcodenum
+  if (script.match(hexchars) !== null) {
+    const e = new Error('Must provide a valid hexadecimal script')
+    e.code = 'ERR_INVALID_SCRIPT'
+    throw e
+  }
 
-        if (!nextOpcode || nextOpcode.length === 0) {
-          const e = new Error('Must provide an OPCODE')
-          e.code = 'ERR_INVALID_OPCODE'
-          throw e
-        }
+  const parsedScript = new bsv.Script(Buffer.from(script, 'hex'))
+  const lockingPublicKey = parsedScript.chunks[0].buf.toString('hex')
+  const fields = []
+  const acceptedFormats = ['hex', 'base64', 'utf8', 'buffer']
+  let signature
 
-        // If the next value is DROP or 2DROP then this is the signature
-        if (nextOpcode === OP_DROP || nextOpcode === OP_2DROP) {
-          signature = parsedScript.chunks[i].buf.toString('hex')
-          break
-        }
+  for (let i = 2; i < parsedScript.chunks.length; i++) {
+    const nextOpcode = parsedScript.chunks[i + 1].opcodenum
 
-        let chunk = parsedScript.chunks[i].buf
-        if (!chunk) {
-          if (parsedScript.chunks[i].opcodenum >= 80 && parsedScript.chunks[i].opcodenum <= 95) {
-            chunk = Buffer.from([parsedScript.chunks[i].opcodenum - 80])
-          }
-        }
-
-        if (!fieldFormat || acceptedFormats.includes(fieldFormat) == false) {
-          const e = new Error('Must provide a field format value')
-          e.code = 'ERR_INVALID_FIELD_FORMAT'
-          throw e
-        }
-
-        if (fieldFormat === 'buffer') {
-          fields.push(chunk)
-        } else {
-          fields.push(chunk.toString(fieldFormat))
-        }
-      }
-      return {
-        fields,
-        lockingPublicKey,
-        signature
-      }
-    } else {
-      const e = new Error('Must provide a valid hexadecimal script')
-      e.code = 'ERR_INVALID_SCRIPT'
+    if (typeof nextOpcode !== 'number') {
+      const e = new Error('There is no opcode for a chunk immediately following the last push. This is not a valid PushDrop token.')
+      e.code = 'ERR_INVALID_OPCODE'
       throw e
     }
-  } catch (error) {
-    console.log(error)
+
+    // If the next value is DROP or 2DROP then this is the signature
+    if (nextOpcode === OP_DROP || nextOpcode === OP_2DROP) {
+      signature = parsedScript.chunks[i].buf.toString('hex')
+      break
+    }
+
+    let chunk = parsedScript.chunks[i].buf
+    if (!chunk) {
+      if (parsedScript.chunks[i].opcodenum >= 80 && parsedScript.chunks[i].opcodenum <= 95) {
+        chunk = Buffer.from([parsedScript.chunks[i].opcodenum - 80])
+      }
+    }
+
+    if (!fieldFormat || !acceptedFormats.includes(fieldFormat)) {
+      const e = new Error('Must provide a field format value')
+      e.code = 'ERR_INVALID_FIELD_FORMAT'
+      throw e
+    }
+
+    if (fieldFormat === 'buffer') {
+      fields.push(chunk)
+    } else {
+      fields.push(chunk.toString(fieldFormat))
+    }
+  }
+
+  return {
+    fields,
+    lockingPublicKey,
+    signature
   }
 }
